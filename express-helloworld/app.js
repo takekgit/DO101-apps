68b949dca4cf99a9c90ea86c6d0c9489b28f58cf
keiji.takeda1@ibm.com <takedakeiji@takedakeishinoMacBook-Air.local>
2020-05-25 19:05:57 +0900

    Changed

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,10 +9,6 @@ app.get('/mars', function (req, res) {
   res.send('Hello Mars!\n');
 });
 
-app.get('/Jupiter', function (req, res) {
-  res.send('Hello Jupiter!\n');
-});
-
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
